ca4d992984353176988a5d19ef32391170f0a21c
theDoubleLink <brandtbandstra@gmail.com>
2025-09-17 09:45:10 -0500

    Completed the first 3 questions

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
+let favoriteFoods = ["Pizza","Sushi","Coke","Ice Cream","Soft Pretzel","Pineapple" ]
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
-
-
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log("One of my favorite foods is " + favoriteFoods[i]);
+}
 
 // 3. Print out the rating for each food with a ranking like:
 // "My #1 favorite food is Ramen" (copy/paste for all items)
 // "My #2 favorite food is Sushi"
 // ...etc.
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log("My #" + (i+1) + " is " + favoriteFoods[i]);
+}
 
 
 
